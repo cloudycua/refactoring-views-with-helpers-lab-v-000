2dc185b272f76f507815944fe8a7542f85252778
Christine Cua <cloudycua@gmail.com>
2018-10-03 19:57:18 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,8 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
+# allows an Artist to be retrieved from and associated
+# with a Song instance
   def artist_name
     artist.name if artist
   end
